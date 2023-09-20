3a2aa753dd7869b857433690a697e2beb1845824
Simon210805 <yohanness803@gmail.com>
2023-09-20 22:40:11 +0200

    add

--- script.js
+++ script.js
@@ -2,16 +2,46 @@ let resultHeader =document.querySelector("span");
 let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
 let addButton = document.getElementById("add")
-let sub = document.getElementById("sub")
-let malti = document.getElementById("malti")
-let divi = document.getElementById("divi")
+let subButton = document.getElementById("sub")
+let maltiButton = document.getElementById("malti")
+let diviButton = document.getElementById("divi")
 function addition(a,b){
     return a+b;
 }
-add.onclick =()=>{ 
+addButton.onclick =()=>{ 
     let convertednum1 = parseInt(num1.value)
     let convertednum2 = parseInt(num2.value)
     if (!isNaN(convertednum1) && !isNaN(convertednum2))
    { let result = addition (convertednum1, convertednum2)
+    resultHeader.innerText = result;}
+}
+function maltiplication(a,b){
+    return a+b;
+}
+maltiButton.onclick =()=>{ 
+    let convertednum1 = parseInt(num1.value)
+    let convertednum2 = parseInt(num2.value)
+    if (!isNaN(convertednum1) && !isNaN(convertednum2))
+   { let result = maltiplication (convertednum1, convertednum2)
+    resultHeader.innerText = result;}
+}
+function subtraction(a,b){
+    return a+b;
+}
+subButton.onclick =()=>{ 
+    let convertednum1 = parseInt(num1.value)
+    let convertednum2 = parseInt(num2.value)
+    if (!isNaN(convertednum1) && !isNaN(convertednum2))
+   { let result = subtraction (convertednum1, convertednum2)
+    resultHeader.innerText = result;}
+}
+function divided(a,b){
+    return a+b;
+}
+diviButton.onclick =()=>{ 
+    let convertednum1 = parseInt(num1.value)
+    let convertednum2 = parseInt(num2.value)
+    if (!isNaN(convertednum1) && !isNaN(convertednum2))
+   { let result = divided (convertednum1, convertednum2)
     resultHeader.innerText = result;}
 }
